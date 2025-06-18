2c68f2405f4435d06b9034cac8611d8888358e80
Charlie Wunderlich <cww324@gmail.com>
2025-06-18 16:14:55 -0500

    so somehow lost PostManager logic, but I believe this should work with all the posts, maaaybe need to fix things I dunno

--- src/managers/PostManager.js
+++ src/managers/PostManager.js
@@ -8,3 +8,29 @@ export const createPost = (postData, token) => {
     body: JSON.stringify(postData),
   }).then((res) => res.json())
 }
+
+export const getAllPosts = () => {
+  return fetch("http://localhost:8088/posts").then((res) => res.json())
+}
+
+export const getPostById = (postId) => {
+  return fetch(`http://localhost:8088/posts/${postId}`).then((res) =>
+    res.json()
+  )
+}
+
+export const updatePost = (postId, updatedPost) => {
+  return fetch(`http://localhost:8088/posts/${postId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedPost),
+  })
+}
+
+export const deletePost = (postId) => {
+  return fetch(`http://localhost:8088/posts/${postId}`, {
+    method: "DELETE",
+  })
+}
