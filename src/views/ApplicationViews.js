856f65cdd5fb90f152bf483d37b8ad203d29bd22
Charlie Wunderlich <cww324@gmail.com>
2025-06-18 10:45:59 -0500

    Front-end Wiring up of Post Details - added route in ApplicaitonViews, and created PostDetails.jsx page

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -3,16 +3,18 @@ import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 import { Authorized } from "./Authorized"
 import { CreatePost } from "../components/posts/CreatePost.jsx"
+import { PostDetails } from "../components/posts/PostDetails.jsx"
 
 export const ApplicationViews = ({ token, setToken }) => {
-  return <>
+  return (
     <Routes>
       <Route path="/login" element={<Login setToken={setToken} />}  />
       <Route path="/register" element={<Register setToken={setToken} />}  />
       <Route element={<Authorized token={token} />}>
         {/* Add Routes here */}
-        <Route path="createpost" element={<CreatePost token={token} />} />
+        <Route path="/createpost" element={<CreatePost token={token} />} />
+        <Route path="/posts/:postId" element={<PostDetails />} />
       </Route>
     </Routes>
-  </>
+  )
 }
