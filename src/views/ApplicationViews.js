289f9f61a4038225f7e1a68b44c79fe971687da8
Charlie Wunderlich <cww324@gmail.com>
2025-06-23 14:55:10 -0500

    adding UserManager.js, /users/ folder, and modifying login/navbar/applicationViews.js to allow User Profiles link to show in NavBar and so an admin can see all user profiles

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -12,6 +12,7 @@ import { TagManager } from "../components/tagmanager/tagmanager.jsx"
 import { CategoryManager } from "../components/categories/CatgeoryDetails.jsx"
 
 import { CommentForm } from "../components/comments/CommentForm.jsx"
+import { UserProfiles } from "../components/users/UserProfiles.jsx"
 
 export const ApplicationViews = ({ user, setUser }) => {
   return (
@@ -38,6 +39,9 @@ export const ApplicationViews = ({ user, setUser }) => {
         <Route path="/tagmanager/edittag/:id" element={<TagManager user={user} />} />
         <Route path="/categorymanager" element={<CategoryManager user={user} />} />
         <Route path="/categorymanager/editcategory/:id" element={<TagManager user={user} />} />
+
+        {/* User Profiles and Stuff */}
+        <Route path="/user-profiles" element={<UserProfiles user={user}/>} />
       </Route>
     </Routes>
   )
