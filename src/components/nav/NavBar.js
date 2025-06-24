b9721ce1d66c6bf407befb51dce81ec20245a420
Jon Young <youngt.jonathan>
2025-06-24 09:57:04 -0500

    updates

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,20 +1,25 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./NavBar.css";
 import Logo from "./rare.jpeg";
 
 export const NavBar = ({ user, setUser }) => {
-  console.log(" Logged in User", user)
-  const navigate = useNavigate()
-  const navbar = useRef()
-  const hamburger = useRef()
+  const navigate = useNavigate();
+  const navbar = useRef();
+  const hamburger = useRef();
+  const [darkMode, setDarkMode] = useState(
+    document.body.classList.contains("dark-mode")
+  );
 
   const showMobileNavbar = () => {
     hamburger.current.classList.toggle("is-active");
     navbar.current.classList.toggle("is-active");
   };
 
-
+  const toggleDarkMode = () => {
+    document.body.classList.toggle("dark-mode");
+    setDarkMode(!darkMode);
+  };
 
   return (
     <nav
@@ -24,20 +29,10 @@ export const NavBar = ({ user, setUser }) => {
     >
       <div className="navbar-brand">
         <a className="navbar-item" href="/">
-          <img src={Logo} height="3rem" alt="Rare Logo" />{" "}
-          <h1 className="title is-4">Rare Publishing</h1>
+          <img src={Logo} height="3rem" alt="Rare Logo" />
+          <h1 className="title is-4 ml-2">Rare Publishing</h1>
         </a>
 
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a
-          role="button"
-          className="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-          onClick={showMobileNavbar}
-          ref={hamburger}
-        >
         <a
           role="button"
           className="navbar-burger"
@@ -54,68 +49,44 @@ export const NavBar = ({ user, setUser }) => {
 
       <div className="navbar-menu" ref={navbar}>
         <div className="navbar-start">
-          {user ? (
-            <Link to="/allposts" className="navbar-item">
-              All Posts
-            </Link>
-          ) : (
-            ""
+          {user && (
+            <>
+              <Link to="/allposts" className="navbar-item">
+                All Posts
+              </Link>
+              <Link to="/" className="navbar-item">
+                My Posts
+              </Link>
+              <Link to="/categorymanager" className="navbar-item">
+                Category Manager
+              </Link>
+              <Link to="/tagmanager" className="navbar-item">
+                Tag Manager
+              </Link>
+              <Link to="/" className="navbar-item">
+                User Manager
+              </Link>
+            </>
           )}
-        </div>
-        <div className="navbar-start">
-          {user ? (
-            <Link to="/myposts" className="navbar-item">
-              My Posts
+          {user?.isStaff && (
+            <Link to="/user-profiles" className="navbar-item">
+              User Profiles
             </Link>
-          ) : (
-            ""
           )}
-        </div>
-        <div className="navbar-start">
-          {user ? (
-            <Link to="categorymanager" className="navbar-item">
-              Category Manager
-            </Link>
-          ) : (
-            ""
-          )}
-        </div>
-        <div className="navbar-start">
-          {user ? (
-            <Link to="tagmanager" className="navbar-item">
-              Tag Manager
-            </Link>
-          ) : (
-            ""
-          )}
-        </div>
-        <div className="navbar-start">
-          {user ? (
-            <Link to="/usermanager" className="navbar-item">
-              User Manager
-            </Link>
-          ) : (
-            ""
-          )}
-          {user && <Link to="/allposts" className="navbar-item">All Posts</Link>}
-          {user && <Link to="/" className="navbar-item">My Posts</Link>}
-          {user && <Link to="/categorymanager" className="navbar-item">Category Manager</Link>}
-          {user && <Link to="/tagmanager" className="navbar-item">Tag Manager</Link>}
-          {user && <Link to="/" className="navbar-item">User Manager</Link>}
-          {user?.isStaff && (<Link to="/user-profiles" className="navbar-item">User Profiles</Link>)}
         </div>
 
         <div className="navbar-end">
           <div className="navbar-item">
-            <div className="nuttons">
+            <div className="buttons are-small">
               <button
-              className="button is-outlined"
-                onClick={() => document.body.classList.toggle("dark-mode")}
+                className={`button ${
+                  darkMode ? "is-light has-text-dark" : "is-dark has-text-light"
+                }`}
+                onClick={toggleDarkMode}
               >
-                Toggle Dark Mode
+                {darkMode ? "☀️ Light Mode" : "🌙 Dark Mode"}
               </button>
-            </div>
-            <div className="buttons">
+
               {user ? (
                 <button
                   className="button is-outlined"
@@ -136,19 +107,6 @@ export const NavBar = ({ user, setUser }) => {
                   </Link>
                 </>
               )}
-              {user ? (
-                <button className="button is-outlined" onClick={() => {
-                  setUser('')
-                  navigate('/login')
-                }}>
-                  Logout
-                </button>
-              ) : (
-                <>
-                  <Link to="/register" className="button is-link">Register</Link>
-                  <Link to="/login" className="button is-outlined">Login</Link>
-                </>
-              )}
             </div>
           </div>
         </div>
@@ -156,3 +114,161 @@ export const NavBar = ({ user, setUser }) => {
     </nav>
   );
 };
+
+
+// import { useRef } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import "./NavBar.css";
+// import Logo from "./rare.jpeg";
+
+// export const NavBar = ({ user, setUser }) => {
+//   console.log(" Logged in User", user)
+//   const navigate = useNavigate()
+//   const navbar = useRef()
+//   const hamburger = useRef()
+
+//   const showMobileNavbar = () => {
+//     hamburger.current.classList.toggle("is-active");
+//     navbar.current.classList.toggle("is-active");
+//   };
+
+//   return (
+//     <nav
+//       className="navbar is-success mb-3"
+//       role="navigation"
+//       aria-label="main navigation"
+//     >
+//       <div className="navbar-brand">
+//         <a className="navbar-item" href="/">
+//           <img src={Logo} height="3rem" alt="Rare Logo" />{" "}
+//           <h1 className="title is-4">Rare Publishing</h1>
+//         </a>
+
+//         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+//         <a
+//           role="button"
+//           className="navbar-burger"
+//           aria-label="menu"
+//           aria-expanded="false"
+//           data-target="navbarBasicExample"
+//           onClick={showMobileNavbar}
+//           ref={hamburger}
+//         >
+//         <a
+//           role="button"
+//           className="navbar-burger"
+//           aria-label="menu"
+//           aria-expanded="false"
+//           onClick={showMobileNavbar}
+//           ref={hamburger}
+//         >
+//           <span aria-hidden="true"></span>
+//           <span aria-hidden="true"></span>
+//           <span aria-hidden="true"></span>
+//         </a>
+//       </div>
+
+//       <div className="navbar-menu" ref={navbar}>
+//         <div className="navbar-start">
+//           {user ? (
+//             <Link to="/allposts" className="navbar-item">
+//               All Posts
+//             </Link>
+//           ) : (
+//             ""
+//           )}
+//         </div>
+//         <div className="navbar-start">
+//           {user ? (
+//             <Link to="/myposts" className="navbar-item">
+//               My Posts
+//             </Link>
+//           ) : (
+//             ""
+//           )}
+//         </div>
+//         <div className="navbar-start">
+//           {user ? (
+//             <Link to="categorymanager" className="navbar-item">
+//               Category Manager
+//             </Link>
+//           ) : (
+//             ""
+//           )}
+//         </div>
+//         <div className="navbar-start">
+//           {user ? (
+//             <Link to="tagmanager" className="navbar-item">
+//               Tag Manager
+//             </Link>
+//           ) : (
+//             ""
+//           )}
+//         </div>
+//         <div className="navbar-start">
+//           {user ? (
+//             <Link to="/usermanager" className="navbar-item">
+//               User Manager
+//             </Link>
+//           ) : (
+//             ""
+//           )}
+//           {user && <Link to="/allposts" className="navbar-item">All Posts</Link>}
+//           {user && <Link to="/" className="navbar-item">My Posts</Link>}
+//           {user && <Link to="/categorymanager" className="navbar-item">Category Manager</Link>}
+//           {user && <Link to="/tagmanager" className="navbar-item">Tag Manager</Link>}
+//           {user && <Link to="/" className="navbar-item">User Manager</Link>}
+//           {user?.isStaff && (<Link to="/user-profiles" className="navbar-item">User Profiles</Link>)}
+//         </div>
+
+//         <div className="navbar-end">
+//           <div className="navbar-item">
+//             <div className="buttons">
+//               <button
+//               className="button is-outlined"
+//                 onClick={() => document.body.classList.toggle("dark-mode")}
+//               >
+//                 Toggle Dark Mode
+//               </button>
+//             </div>
+//             <div className="buttons">
+//               {user ? (
+//                 <button
+//                   className="button is-outlined"
+//                   onClick={() => {
+//                     setUser("");
+//                     navigate("/login");
+//                   }}
+//                 >
+//                   Logout
+//                 </button>
+//               ) : (
+//                 <>
+//                   <Link to="/register" className="button is-link">
+//                     Register
+//                   </Link>
+//                   <Link to="/login" className="button is-outlined">
+//                     Login
+//                   </Link>
+//                 </>
+//               )}
+//               {user ? (
+//                 <button className="button is-outlined" onClick={() => {
+//                   setUser('')
+//                   navigate('/login')
+//                 }}>
+//                   Logout
+//                 </button>
+//               ) : (
+//                 <>
+//                   <Link to="/register" className="button is-link">Register</Link>
+//                   <Link to="/login" className="button is-outlined">Login</Link>
+//                 </>
+//               )}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
